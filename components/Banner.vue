f53f8d1e96469e0b7d47c7c759c34aa5369b08c4
Murod <murodjon.umarovv@gmail.com>
2022-03-24 20:15:22 +0500

    banner

---
+++ components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="height: 80vh">
+    <div
+      class="flex lg:flex-row md:flex-col sm:flex-col h-full lg:items-center container mx-auto"
+    >
+      <div class="lg:w-1/2 w-full flex flex-col gap-y-8">
+        <div class="font-bold text-6xl" style="letter-spacing: -4px">
+          Попробуйте
+          <span class="orange"> неделю </span>
+          правильного питания
+        </div>
+        <div class="text-lg font-semibold">
+          Правильная еда по подписке на каждый день
+        </div>
+        <div class="flex items-center gap-x-8">
+          <del class="text-3xl text-gray-300">6 390$</del>
+          <b class="text-4xl">от 5 544$</b>
+        </div>
+        <div>
+          <button class="bg-orange text-white text-xl font-semibold rounded-full px-20 py-5">
+            Попробовать
+          </button>
+        </div>
+      </div>
+
+      <div class="lg:w-1/2 w-full">
+        <span>
+          <img
+            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dcaf1af5-3162-42f2-a863-b4ce4d9d11ec/dd3d416-326cc8e4-ec52-451f-bcd2-5fba96dea4b3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjYWYxYWY1LTMxNjItNDJmMi1hODYzLWI0Y2U0ZDlkMTFlY1wvZGQzZDQxNi0zMjZjYzhlNC1lYzUyLTQ1MWYtYmNkMi01ZmJhOTZkZWE0YjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1t57IcCD5NWJ8yUKn4Q1iAwvJTa5g8-jvhicNz9R6fQ"
+            class="w-full"
+            alt=""
+          />
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
