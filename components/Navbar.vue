3c2fb4be6754543c51e98ddd02ac1286c9c1fc5e
Murod <murodjon.umarovv@gmail.com>
2022-03-24 20:15:14 +0500

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="height: 20vh;">
+    <nav class="flex p-10 px-14 justify-between items-center">
+      <ul class="flex lg:gap-x-10 text-xl font-medium w-2/5">
+        <li class="text-hover cursor-pointer">Меню и цены</li>
+        <li class="text-hover cursor-pointer">Доставка</li>
+        <li class="text-hover cursor-pointer">FAQ</li>
+      </ul>
+
+      <div
+        class="text-4xl font-bold "
+      >
+        Patron<sub class="text-gray-500 text-lg">ERP</sub>
+      </div>
+      <ul class="w-2/5"></ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
