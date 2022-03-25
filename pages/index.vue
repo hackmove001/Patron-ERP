089528325eccfbcd006766854b27c03a7538fbc5
Murod <murodjon.umarovv@gmail.com>
2022-03-25 23:06:49 +0500

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,18 @@
   <div>
     <Navbar />
     <Banner />
+    <Advantages />
+    <Content />
+    <Info />
+   
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {}
+  },
 }
 </script>
