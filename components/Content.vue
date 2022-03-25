8e2ad93e85887d90b4513ce28d074ee26668d34e
Murod <murodjon.umarovv@gmail.com>
2022-03-25 23:04:29 +0500

    content

---
+++ components/Content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="text-center">
+      <div
+        class="text-center py-4 text-6xl font-bold"
+        style="letter-spacing: -4px"
+      >
+      Available integration services
+      </div>
+      <div class="text-2xl font-semibold">7 days, 24 hours</div>
+      <div class="text-gray-500 font-semibold pt-3">
+        ККАЛ • <b>1 532 </b> БЕЛКИ • <b>89,91</b> г ЖИРЫ • <b>81,24</b> г
+        УГЛЕВОДЫ • <b>142,94</b> г
+      </div>
+    </div>
+
+    <Slider />
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
